390963d8ed30a91086f152b2f5f38a2d60ae8be8
SerzhantovDimitry <serzhantovdimitri@yandex.ru>
2023-08-12 06:43:22 +0300

    Настройка

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://localhost:3000';
+export const BASE_URL = 'https://api.serzhantov.diplom.nomoredomains.xyz';
 function getResponseData(res) {
   return res.ok ? res.json() : Promise.reject(`Ошибка: ${res.status}`);
 }
