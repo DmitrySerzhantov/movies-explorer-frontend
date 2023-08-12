5e2504c9957ce600fedd9fb35c3a5e9b9b9b05cf
SerzhantovDimitry <serzhantovdimitri@yandex.ru>
2023-08-12 06:37:24 +0300

    Настройка

---
+++ src/utils/constans.js
@@ -0,0 +1,19 @@
+export const itemNavigation = [
+  {value: 'Главная', href: '/', id: '/'},
+  {value: 'Фильмы', href: '/movies', id: '/movies'},
+  {value: 'Сохранённые фильмы', href: '/saved-movies', id: '/saved-movies'},
+];
+export const portfolioItems = [
+  {
+    name: 'Статичный сайт',
+    link: 'https://github.com/DmitrySerzhantov/how-to-learn',
+  },
+  {
+    name: 'Адаптивный сайт',
+    link: 'https://github.com/DmitrySerzhantov/russian-travel',
+  },
+  {
+    name: 'Одностраничное приложение',
+    link: 'https://github.com/DmitrySerzhantov/react-mesto-api-full-gha',
+  },
+];
